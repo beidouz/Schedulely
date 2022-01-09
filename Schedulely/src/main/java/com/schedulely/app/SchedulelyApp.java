a2bf415f3e24c632fe6ea336094cb6c3caa1bb45
Beidou Zhang <beidouz@hotmail.com>
2022-01-08 20:16:24 -0500

    first commit: set up SpringBoot project

---
+++ Schedulely/src/main/java/com/schedulely/app/SchedulelyApp.java
@@ -0,0 +1,13 @@
+package com.schedulely.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulelyApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulelyApp.class, args);
+    }
+
+}
