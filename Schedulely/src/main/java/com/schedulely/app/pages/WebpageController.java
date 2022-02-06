a1fbfc90655e7c4251bb20a114284b9a1fb933a4
William <zhaiwilliam@gmail.com>
2022-02-05 19:20:41 -0500

    added events table to admin page

--- Schedulely/src/main/java/com/schedulely/app/pages/WebpageController.java
+++ Schedulely/src/main/java/com/schedulely/app/pages/WebpageController.java
@@ -1,10 +1,13 @@
 package com.schedulely.app.pages;
 
+import com.schedulely.app.event.Event;
 import com.schedulely.app.event.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class WebpageController {
@@ -18,7 +21,8 @@ public class WebpageController {
 
     @RequestMapping(path = "/admin")
     public String admin(Model model) {
-        model.addAttribute("events", eventService.getAllEvents());
+        List<Event> events = eventService.getAllEvents();
+        model.addAttribute("events", events);
         return "admin";
     }
 
