c611f13e2a6e7e2ed75a04c1c3bec05797770248
William <zhaiwilliam@gmail.com>
2022-02-12 21:21:10 -0500

    added create event form

--- Schedulely/src/main/java/com/schedulely/app/pages/WebpageController.java
+++ Schedulely/src/main/java/com/schedulely/app/pages/WebpageController.java
@@ -5,7 +5,9 @@ import com.schedulely.app.entities.event.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -28,12 +30,14 @@ public class WebpageController {
 
     @RequestMapping(path={"", "/", "home"})
     public String home(Model model) {
+        model.addAttribute("event", new Event());
         return "home";
     }
 
-//    @RequestMapping(path={"*"})
-//    public String notFound(Model model) {
-//        return "not-found";
-//    }
+    @RequestMapping(value = "/createEvent", method= RequestMethod.POST)
+    public String createEvent(@ModelAttribute Event event) {
+        this.eventService.addNewEvent(event);
+        return "redirect:admin"; //Todo: redirect to that event page
+    }
 
 }
