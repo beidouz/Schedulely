6187307d8003590eedba9d2343f2f5b62601b43c
William <zhaiwilliam@gmail.com>
2022-02-19 13:07:34 -0500

    reset demo data, also remember to review application.properties

--- Schedulely/src/main/java/com/schedulely/app/config/DemoDataConfig.java
+++ Schedulely/src/main/java/com/schedulely/app/config/DemoDataConfig.java
@@ -17,17 +17,20 @@ public class DemoDataConfig {
     @Bean
     CommandLineRunner commandLineRunner(EventRepository eventRepository, AvailabilityRepository availabilityRepository) {
         return args -> {
-            Event e1 = new Event("Move night", "Lets set a time to watch the movie tonight! When is everyone available?");
+            Event e1 = new Event("Demo Event", "This is the event's description!");
             Event e2 = new Event("Team Meeting", "Hey team, lets set a time to meet tomorrow in preparation for the game this weekend.");
-            Event e3 = new Event("Fishing", "when can yall get up tmr?");
             eventRepository.saveAll(
-                    Arrays.asList(e1, e2, e3)
+                    Arrays.asList(e1, e2)
             );
 
-            Availability e1a1 = new Availability("Bob", LocalTime.of(20, 30, 0), 1L);
-            Availability e1a2 = new Availability("Doug", LocalTime.of(21, 00, 0), 1L);
+            Availability e1a1 = new Availability("User 2", LocalTime.of(9, 30, 0), e1.getId());
+            Availability e1a2 = new Availability("User 5", LocalTime.of(10, 30, 0), e1.getId());
+
+            Availability e2a1 = new Availability("Bjergsen", LocalTime.of(20, 30, 0), e2.getId());
+            Availability e2a2 = new Availability("Bwipo", LocalTime.of(21, 0, 0), e2.getId());
+            Availability e2a3 = new Availability("Steve", LocalTime.of(21, 0, 0), e2.getId());
             availabilityRepository.saveAll(
-                    Arrays.asList(e1a1, e1a2)
+                    Arrays.asList(e1a1, e1a2, e2a1, e2a2, e2a3)
             );
 
         };
